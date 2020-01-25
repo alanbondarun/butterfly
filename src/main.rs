d65518ff3cc1e0a02fb16250b970ccdde088630e
alanb <efik722life@gmail.com>
2020-01-25 19:27:59 +0900

    feat: add tokio to spawn & collect tasks asynchronously

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let total_users = 10000;
+    let concurrent_users = 10;
+
+    let mut rt = tokio::runtime::Builder::new()
+        .threaded_scheduler()
+        .core_threads(concurrent_users)
+        .build()?;
+    rt.block_on(async {
+        let futures = std::iter::repeat(())
+            .take(total_users)
+            .enumerate()
+            .map(|(idx, _)| {
+                tokio::spawn(async move {
+                    println!("task #{} done.", idx);
+                })
+            })
+            .collect::<Vec<tokio::task::JoinHandle<()>>>();
+
+        for future in futures {
+            future.await.expect("Error");
+        }
+    });
+
+    Ok(())
 }
