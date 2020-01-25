b10ce545e780e43829c90f0a623f14f4d7746879
alanb <efik722life@gmail.com>
2020-01-26 00:55:21 +0900

    feat: add HTTP request task

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let total_users = 10000;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+struct RequestResult {
+    _status_code: hyper::StatusCode,
+    _response_time: f64,
+}
+
+fn main() -> Result<()> {
+    let total_users = 100;
     let concurrent_users = 10;
 
     let mut rt = tokio::runtime::Builder::new()
         .threaded_scheduler()
         .core_threads(concurrent_users)
+        .enable_all()
         .build()?;
     rt.block_on(async {
         let futures = std::iter::repeat(())
             .take(total_users)
-            .enumerate()
-            .map(|(idx, _)| {
-                tokio::spawn(async move {
-                    println!("task #{} done.", idx);
-                })
-            })
-            .collect::<Vec<tokio::task::JoinHandle<()>>>();
+            .map(|_| tokio::spawn(async move { make_request().await }))
+            .collect::<Vec<tokio::task::JoinHandle<Result<RequestResult>>>>();
 
         for future in futures {
-            future.await.expect("Error");
+            future.await.expect("Error").expect("Error");
         }
     });
 
     Ok(())
+}
+
+async fn make_request() -> Result<RequestResult> {
+    let endpoint: hyper::Uri = "http://127.0.0.1:8080/api/task".parse()?;
+
+    let client = hyper::Client::new();
+
+    let now = std::time::SystemTime::now();
+    let response = client.get(endpoint).await?;
+    let elapsed_time = now.elapsed()?.as_secs_f64();
+
+    Ok(RequestResult {
+        _status_code: response.status(),
+        _response_time: elapsed_time,
+    })
 }
